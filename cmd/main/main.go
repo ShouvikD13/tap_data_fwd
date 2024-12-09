a0306f95a93c69c751f93996e2755493a21a3cd7
devudubey15 <devdubey15091999@gmail.com>
2024-12-09 17:01:48 +0530

    First commit - 09/Dec/2024 : Checksum Issue is resolved

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -40,7 +40,7 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	wg.Add(2)
+	wg.Add(1)
 
 	go func() {
 		defer wg.Done()
@@ -50,6 +50,7 @@ func main() {
 		}
 	}()
 
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		if initResult := serviceInitManager.LogOnToTapInitialization(); initResult != 0 {
